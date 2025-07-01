667f24f56bbacb7e15825890374e9f20dda39dbc
FIRST_NAME LAST_NAME <LitCourage>
2025-07-01 18:02:25 +0300

    changes: improved creation of cards images object, removed functions random and shuffle, shortened array shuffling

--- src/main.js
+++ src/main.js
@@ -2,28 +2,20 @@ import './style.css'
 
 // CONSTANTS
 
-const cardImagesObj = {
-    star: '<i class="fa-solid fa-star fa-2x"></i>',
-    cloud: '<i class="fa-solid fa-cloud fa-2x"></i>',
-    phone: '<i class="fa-solid fa-phone fa-2x"></i>',
-    paperclip: '<i class="fa-solid fa-paperclip fa-2x"></i>',
-    bomb: '<i class="fa-solid fa-bomb fa-2x"></i>',
-    heart: '<i class="fa-solid fa-heart fa-2x"></i>',
-    umbrella: '<i class="fa-solid fa-umbrella fa-2x"></i>',
-    gear: '<i class="fa-solid fa-gear fa-2x"></i>',
-    lightning: '<i class="fa-solid fa-bolt fa-2x"></i>',
-    car: '<i class="fa-solid fa-car fa-2x"></i>',
-    gift: '<i class="fa-solid fa-gift fa-2x"></i>',
-    ghost: '<i class="fa-solid fa-ghost fa-2x"></i>',
-    bell: '<i class="fa-solid fa-bell fa-2x"></i>',
-    tree: '<i class="fa-solid fa-tree fa-2x"></i>',
-    sun: '<i class="fa-solid fa-sun fa-2x"></i>',
-    feather: '<i class="fa-solid fa-feather fa-2x"></i>',
-    fish: '<i class="fa-solid fa-fish fa-2x"></i>',
-    drop: '<i class="fa-solid fa-droplet fa-2x"></i>',
-    question: '<i class="fa-solid fa-circle-question fa-2x"></i>'
-};
+const icons = [
+    'star', 'cloud', 'phone', 'paperclip', 'bomb', 'heart',
+    'umbrella', 'gear', 'bolt', 'car', 'gift', 'ghost', 'bell',
+    'tree', 'sun', 'feather', 'fish', 'droplet'
+]
+const cardImagesObj = Object.fromEntries(icons.map(
+    icon => [
+        icon,
+        `<i class="fa-solid fa-${icon} fa-2x"></i>`
+    ]))
+
+const circleQuestion = '<i class="fa-solid fa-circle-question fa-2x"></i>'
 const cardImages = Object.values(cardImagesObj);
+
 const timeLimits = {
     2: 10,
     4: 45,
@@ -56,23 +48,12 @@ function setup() {
     clearInterval(timeCounter);
 }
 
-function addClickListener(selector, callback) {document.querySelector(selector).addEventListener('click', (event) => {callback(event)})}
+function addClickListener(selector, callback) {document.querySelector(selector).addEventListener('click', event => callback(event))}
 
 function changeElementText(selector, text) {document.querySelector(selector).innerText = text}
 
 function isEven(num) {
     return (num % 2 === 0);
-}
-
-function random(min, max) {return min + Math.floor(Math.random() * max)}
-
-function shuffle(array) {
-    let currentIndex = array.length;
-    while (currentIndex !== 0) {
-        let randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
-    }
 }
 
 // GAME FUNCTIONS
@@ -104,8 +85,8 @@ function handleStepEnd(card_el) {
     setTimeout(() => {
         activeCard.classList.remove('game-card-active')
         card_el.classList.remove('game-card-active')
-        card_el.innerHTML = cardImagesObj.question
-        activeCard.innerHTML = cardImagesObj.question
+        card_el.innerHTML = circleQuestion
+        activeCard.innerHTML = circleQuestion
         activeCard = null;
         canClick = true;
     }, 1000);
@@ -118,15 +99,15 @@ function createPair(cardId) {
     card2_el.className = 'game-card centring'
     card1_el.id = `${cardId}`
     card2_el.id = `${cardId + 1}`
-    card1_el.innerHTML = cardImagesObj.question
-    card2_el.innerHTML = cardImagesObj.question
+    card1_el.innerHTML = circleQuestion
+    card2_el.innerHTML = circleQuestion
     card1_el.addEventListener('click', (event) => checkForPair(event.currentTarget))
     card2_el.addEventListener('click', (event) => checkForPair(event.currentTarget))
     return [card1_el, card2_el];
 }
 
 function checkForPair(card_el) {
-    if (card_el === activeCard || !canClick || card_el.classList.contains('game-card-ready') || !canPlay) {return}
+    if (card_el === activeCard || !canClick || !canPlay || card_el.classList.contains('game-card-ready')) {return}
 
     const id = parseInt(card_el.id);
     card_el.classList.add('game-card-active');
@@ -159,18 +140,18 @@ function loadGameField(pairsQuantity) {
     gameField.innerHTML = '';
 
     time = timeLimits[pairsQuantity];
-    maxPairs = pairsQuantity**2 / 2;
+    maxPairs = pairsQuantity ** 2 / 2;
 
     const cardsList = []
 
-    for (let cardId = 0; cardId < pairsQuantity**2; cardId += 2) {
+    for (let cardId = 0; cardId < pairsQuantity ** 2; cardId += 2) {
         const [card1_el, card2_el] = createPair(cardId);
         cardsList.push(card1_el);
         cardsList.push(card2_el);
     }
 
-    shuffle(cardsList);
-    cardsList.forEach((card) => {gameField.append(card)})
+    cardsList.sort(() => Math.random() - 0.5);
+    cardsList.forEach(card => gameField.append(card))
 
     changeElementText('.game-time', `time: ${time}`);
     timeCounter = setInterval(() => {
@@ -192,8 +173,6 @@ function loadStartMenu() {
 
     setup()
 }
-
-
 
 document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault()
